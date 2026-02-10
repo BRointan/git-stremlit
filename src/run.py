af1929a520ddf708a29c11e787a908a8e020e905
BRointan <3mr3.sec@gmail.com>
2026-02-10 22:26:21 +0330

    create first code

---
+++ src/run.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+st.title(":zap: Admin dahsboard")
+st.divider()
+st.badge("New", color="green", icon=":material/new_label:", width="content")
+
+st.html(
+    "<p><span style='text-decoration: line-through double red;'>Oops</span>!</p>"
+)
+
